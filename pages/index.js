6fc44c482d6f44b6adbe3f9596790da91dbbb7d5
Matheus Almeida <43671640+tthheusalmeida@users.noreply.github.com>
2024-03-13 14:32:53 +0000

    feat: create home page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Estou enfrentando adversidades desde o dia em que nasci! Não importa quantos de vocês existam ou o que aconteça, vou superar tudo. - (Asta)</h1>;
+}
+
+export default Home;
